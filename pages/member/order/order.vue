bbb76082872aa1fb1bea64af105e7c74d6bb7002
SmileBX <996889692@qq.com>
2020-03-25 18:47:35 +0800

    m订单详情 我的订单 投诉建议

---
+++ pages/member/order/order.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="order">
+		<view class="bg_fff tabList flex">
+			<view v-for="(item,index) in tabList" :key="index" class="item" :class="{'active':index==1}">{{item.name}}</view>
+		</view>
+		<view class="list pw3">
+			<!--
+			等待买家付款:取消订单 立即支付
+			买家已付款/交易关闭:无状态
+			卖家已发货：查看物流 确认收货
+			交易成功：查看物流 评价
+			退款成功/退款中：退款详情
+			-->
+			<view class="order_item bg_fff mt2" v-for="(item,key) in 4" :key="key">
+				<view class="flex justifyContentBetween flexAlignCenter">
+					<view>
+						<image src="../../../static/my/shop.png" class="logo"></image>
+						<text class="uni-bold shop_name">炫宝迪旗舰店</text>
+						<text class="iconfont icon-arrow_r font18"></text>
+					</view>
+					<view class="color_red font18">等待买家付款</view>
+				</view>
+				<view class="flex justifyContentBetween mt2">
+					<image src="../../../static/of/4.png" class="img mr2"></image>
+					<view class="flex1 order_info">
+						<view>ICL晶体植入术</view> 
+						<view class="color_gray font18">杭州芯美昕医疗美容门诊</view> 
+						<view class="flex justifyContentBetween flexAlignCenter mt1">
+							<text class="color_gray">x1</text>
+							<text>¥6800</text>
+						</view>
+					</view>
+				</view>
+				<view class="text_right mt2">总计：¥<span class="font32 uni-bold">6800.00</span></view>
+				<view class="btn flex justifyContentEnd">
+					<view class="btn_g">取消订单</view>
+					<view class="btn_r">立即支付</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				tabList:[{id:1,name:'全部'},{id:2,name:'待付款'},{id:3,name:'待发货'},{id:4,name:'待收货'},{id:5,name:'待评价'}]
+			}
+		}
+	}
+	
+</script>
+
+<style lang="scss" scoped>
+	@import './style'
+</style>
---
+++ pages/member/orderDetail/orderDetail.vue
@@ -0,0 +1,127 @@
+<template>
+	<view class="order_detail">
+		<view style="width: 100%;background:#ff3737;" :style="{'height':barHeight+'px'}"></view>
+		<view class="top-box">
+			<view class="p2">
+				<span class="iconfont icon-arrow_r"></span>
+			</view>
+			<view class="order_statu mt1">
+				<view class="statu uni-bold">待发货</view>
+				<view class="font18">订单已支付，等待商家发货</view>
+			</view>		
+		</view>
+		<view class="pw3 all_top">
+			<!--地址-->
+			<view class="site flex flexAlignCenter">
+				<image src="../../../static/my/dz.png" class="dz"></image>
+				<view class="font18 flex1">
+					<view>刘华强 <span class="phone">131****2626</span></view>
+					<view>广东 深圳市 龙岗区 坂田街道 雅兰新洲社区学府 花园5期12栋</view>
+				</view>
+			</view>
+			<!--产品信息-->
+			<view class="mt2 bg_fff pro_info">
+				<view>
+					<image src="../../../static/of/shop1.png" class="logo"></image>
+					<text>SKG坚硕专卖店</text>
+				</view>
+				<view class="flex mt2 flexAlignCenter">
+					<image src="../../../static/of/2.png" class="img"></image>
+					<view class="flex1 main">
+						<view class="flex justifyContentBetween">
+							<view class="font18">佳兆业新世界大厦</view>
+							<view class="font18 color_gray">x1</view>
+						</view>
+						<view class="flex flexAlignCenter justifyContentBetween">
+							<view class="flex flexColumn">
+								<view class="font18 color_gray">B1703</view>
+								<view>¥2.8万</view>
+							</view>
+							<view class="font18 color_gray btn_tui">退款</view>
+						</view>
+					</view>
+				</view>
+			</view>
+			<!--订单明细-->
+			<view class="order_info mt2">
+				<view class="info_head">
+					<view class="flex justifyContentBetween">
+						<text>支付方式</text><text>在线支付</text>
+					</view>
+					<view class="mt1 flex justifyContentBetween">
+						<text>商品总价</text><text class="color_red">¥2.8万</text>
+					</view>
+					<view class="mt1 flex justifyContentBetween head_foot">
+						<text>运费</text><text class="color_red">+¥0.00</text>
+					</view>
+				</view>
+				<view class="flex justifyContentBetween mt2">
+					<text>实付</text><text class="color_red">¥2.8万</text>
+				</view>
+			</view>
+			<!--订单明细-->
+			<view class="order_info mt2">
+				<view class="uni-bold">订单信息</view>
+				<view class="mt1">
+					<view class="flex justifyContentBetween">
+						<text>订单编号</text>
+						<view>
+							<text>A1909282128011450560586T2</text>
+							<text class="color_red copy">复制</text>
+						</view>
+					</view>
+					<view class="mt1 flex justifyContentBetween">
+						<text>下单时间</text><text>2019-09-28 21:28:02</text>
+					</view>
+					<view class="mt1 flex justifyContentBetween head_foot flexAlignCenter">
+						<text>买家留言</text>
+						<input type="text" placeholder="请留言" class="text_right font18">
+					</view>
+				</view>
+			</view>
+			<view class="text_center mt2 pp2 bg_fff bor_r15">
+				<text class="iconfont icon-xiaoxi1"></text>
+				<text>联系客服</text>
+			</view>
+			<!--待发货申请退款-->
+			<view class="text_center mt2 pp2 bg_fff bottom" style="display: none;">
+				<text>申请退款</text>
+			</view>
+			<!--
+				待付款：取消订单 立即付款
+				待收货：查看物流 确认收货
+				待评价：查看物流 去评价
+			-->
+			<view class="text_center mt2 bg_fff bottom flex justifyContentBetween dobule">
+				<view>取消订单</view>
+				<view class="btn_add">立即付款</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		data() {
+			return {
+				curPage: "",
+				barHeight:0,
+				userId: "",
+				token: "",
+			}
+		},
+		onLoad() {
+			// #ifdef APP-PLUS
+			var height = plus.navigator.getStatusbarHeight();
+			this.barHeight = height;console.log(this.barHeight)
+			// #endif
+			// #ifdef H5
+			this.barHeight = 0;
+			// #endif
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	@import './style'
+</style>
