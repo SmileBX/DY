eeeeaa7f5489dc129f4713f113541e1068b95542
SmileBX <996889692@qq.com>
2020-03-26 12:08:51 +0800

    设置 优惠券 我的评价 售后服务 申请售后

---
+++ pages/other/account/account.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<view class="flex flexAlignCenter justifyContentBetween item bg_fff mt2">
+			<view>
+				<image src="../../../static/my/wx.png" class="logo mr1"></image>
+				<text>绑定微信</text>
+			</view>
+			<view class="flex flexAlignCenter">
+				<text class="color_gray">已绑定</text>
+				<view class="iconfont icon-arrow_r font26"></view>
+			</view>
+		</view>
+		<view class="flex flexAlignCenter justifyContentBetween item bg_fff">
+			<view>
+				<image src="../../../static/my/sj.png" class="logo mr1"></image>
+				<text>手机号更换</text>
+			</view>
+			<view class="flex flexAlignCenter">
+				<text class="color_gray">13636363636</text>
+				<view class="iconfont icon-arrow_r font26"></view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	
+</script>
+
+<style lang="scss">
+	@import'./style';
+</style>
---
+++ pages/other/set/set.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<view class="flex flexAlignCenter justifyContentBetween item bg_fff mt2">
+			<view>账户和安全</view>
+			<view class="iconfont icon-arrow_r font26"></view>
+		</view>
+		<view class="flex flexAlignCenter justifyContentBetween item bg_fff">
+			<view>清理缓存</view>
+			<view>63.25M</view>
+		</view>
+		<view class="flex flexAlignCenter justifyContentBetween item bg_fff">
+			<view>关于我们</view>
+			<view class="iconfont icon-arrow_r font26"></view>
+		</view>
+		<view class="flex flexAlignCenter justifyContentBetween item bg_fff">
+			<view>软件版本</view>
+			<view>63.25.3</view>
+		</view>
+		<view class="btn_fix">退出登录</view>
+	</view>
+</template>
+
+<script>
+	
+</script>
+
+<style lang="scss">
+	@import'./style';
+</style>
