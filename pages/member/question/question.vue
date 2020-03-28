6fa33c89365f058cab3b9da211d06d68147b8f52
SmileBX <996889692@qq.com>
2020-03-28 16:19:48 +0800

    修改登录密码

--- pages/member/question/question.vue
+++ pages/member/question/question.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="question">
-		<view class="qu_list">
+		<view class="qu_list" v-if="hasData">
 			<view class="qu_item" v-for="(item,index) in 3" :key="index">
 				<view class="flex flexAlignCenter item_head bg_fff">
 					<view class="title flex flexAlignCenter flex1">
@@ -11,11 +11,120 @@
 				</view>
 				<view class="content">这个是内容详情飞飞说法发生法发飞洒发顺丰飞洒发发发生发生发生法发撒噶沙司搞撒嘎嘎啊傻瓜</view>
 			</view>
+			<view class="uni-tab-bar-loading">
+				<uni-load-more :loadingType="loadingType"></uni-load-more>
+			</view>
 		</view>
+		<noData :isShow="noDataIsShow"></noData>
 	</view>
 </template>
 
 <script>
+	import {host,post,get,dateUtils,toLogin,getCurrentPageUrlWithArgs} from '@/common/util.js';
+	import noData from '@/components/noData.vue'; //暂无数据
+	import uniLoadMore from '@/components/uni-load-more.vue'; //加载更多
+	export default{
+		components: {
+			noData,
+			uniLoadMore
+		},
+		data(){
+			return{
+				userId: "",
+				token: "",
+				curPage:"",
+				loadingType: 0, //0加载前，1加载中，2没有更多了
+				isLoad: false,
+				hasData: false,
+				noDataIsShow: false,
+				page: 1,
+				pageSize: 10,
+				allPage: 0,
+				count: 0,
+				datalist:{}
+			}
+		},
+		onShow(){
+			this.userId = uni.getStorageSync("userId");
+			this.token = uni.getStorageSync("token");
+			this.curPage = getCurrentPageUrlWithArgs().replace(/\?/g, '%3F').replace(/\=/g, '%3D').replace(/\&/g, '%26');
+			if (toLogin(this.curPage)) {
+				this.HelpList();
+			}
+			
+			
+		},
+		methods:{
+			async HelpList() {
+				let result = await post("Help/HelpList", {
+					UserId: this.userId,
+					Token: this.token,
+					page: this.page,
+					pageSize: this.pageSize
+				});
+				if (result.code === 0) {
+					let _this=this;
+					if (result.data.length > 0) {
+						this.hasData = true;
+					}else{
+						this.noDataIsShow = true;
+					}
+					this.count = result.count;
+					if (this.count == 0) {
+						
+					}
+					if (parseInt(this.count) % this.pageSize === 0) {
+						this.allPage = this.count / this.pageSize;
+					} else {
+						this.allPage = parseInt(this.count / this.pageSize) + 1;
+					}
+					if (this.page === 1) {
+						this.datalist = result.data;
+					}
+					if (this.page > 1) {
+						this.datalist = this.datalist.concat(
+							result.data
+						);
+					}
+					if (this.allPage <= this.page) {
+						this.isLoad = false;
+						this.loadingType = 2;
+					} else {
+						this.isLoad = true;
+						this.loadingType = 0
+					}
+				} else if (result.code === 2) {
+					let _this =this;
+					uni.showModal({
+						content: "您还没有登录，是否重新登录？",
+						success(res) {
+							if (res.confirm) {
+								uni.navigateTo({
+								  url: "/pages/login/login?askUrl="+_this.curPage
+								});
+							} else if (res.cancel) {
+							}
+						}
+					});
+				} else {
+					uni.showToast({
+						title: result.msg,
+						icon: "none",
+						duration: 2000
+					});
+				}
+			},
+		},
+		onReachBottom: function() {
+			if (this.isLoad) {
+				this.loadingType = 1;
+				this.page++;
+				this.HelpList();
+			} else {
+				this.loadingType = 2;
+			}
+		}
+	}
 </script>
 
 <style lang="scss" scoped>
