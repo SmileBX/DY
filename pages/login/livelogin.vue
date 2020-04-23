efba93b4b2783158991cc461775d3eaa48ba0c58
SmileBX <996889692@qq.com>
2020-04-23 11:58:00 +0800

    直播登录样式调整

--- pages/login/livelogin.vue
+++ pages/login/livelogin.vue
@@ -9,15 +9,16 @@
 				</view>
 			</view>
 			<view class="from pd10">
-				<view class="from-line">
+				<view class="from-line" style="position: relative;">
 					<view :class="['iconfont',logintype?'icon-zh1':'icon-shouji']"></view>
-					<input type="text" class="ipt" v-model="tel" value="" placeholder="请输入手机号码" />
+					<input type="text" class="ipt" v-model="tel" value="" placeholder="请输入手机号码" placeholder-style="font-size:28rpx;font-family:PingFang;font-weight:normal;" />
+					<view class="iconfont icon-cha quxiao_hh" @click="tel = ''"></view>
 				</view>
 				<view class="from-line" v-if="logintype">
 					<view class="iconfont icon-mima"></view>
 					<input type="password" class="ipt" v-model="pwd" value="" placeholder="请输入密码" />
 				</view>
-				<view class="from-line" v-else>
+				<!-- <view class="from-line" v-else>
 					<view class="iconfont icon-mima"></view>
 					<input type="text" class="ipt" value="" v-model="code" placeholder="请输入验证码" />
 					<view class="getcode" @click="getCode">{{codeMsg}}</view>
@@ -25,14 +26,14 @@
 				<view class="from-line text_r" style="margin:0;display: none;" @click="changeWay">
 					<text class="c_theme" v-if="logintype">手机验证码登录</text>
 					<text class="c_theme" v-if="!logintype">密码登录</text>
-				</view>
-				<view class="ftbtn" style="padding:40upx 0 20upx 0;">
+				</view> -->
+				<view class="ftbtn" style="padding:40upx 0 20upx 0;width:80%;">
 					<button type="primary" @click="btnSubmit" class="btn">登录</button>
 				</view>
-				<view class="form-line aLine" style="margin:0 30upx;display: none;">
+				<!-- <view class="form-line aLine" style="margin:0 30upx;display: none;">
 					<view @click="register" class="inline-block aline">没有账号？注册账号</view>
 					<view v-if="logintype" @click="getPassword" class="inline-block aline fr">忘记密码?</view>
-				</view>
+				</view> -->
 			</view>
 		</view>
 	</view>
@@ -121,10 +122,11 @@
 		}
 	}
 </script>
-<style>
+<style lang="scss">
 	@import '../../common/login.css';
 	.logo {
 		padding: 0 0 80upx;
+		margin-left:40upx;
 	}
 	
 	.logo .img {
@@ -156,19 +158,59 @@
 	  width: 90%;
 	  position: absolute;
 	  top: 50%;
-	  left: 5%;
+	  left: 3%;
 	  transform: translateY(-50%);
 	  border-radius: 20rpx;
 	  box-sizing: border-box;
   }
   .from-line{
 	  background: #fff;
-	  border-radius: 40rpx;
+	  width:80%;
+	  margin: 0 auto;
+	  border-radius: 0!important;
+	  position: relative;
+	  .iconfont{
+		  font-size: 16px!important;
+		  background:#e7e6ed;
+		  width:82upx !important;
+	  }
+	  
+  }
+  .iconfont.quxiao_hh{
+	  position: absolute;
+	  left:80%!important;
+	  right:0!important;
+	  top:0!important;
+	  font-size:18px!important;
   }
   .livetitle{
+	  color:#ffffff;
 	  font-size: 32upx;
 	  line-height: 80upx;
 	  text-align: center;
 	  font-weight: 600;
+  }
+  .from{
+	  display: flex;flex-direction: column;justify-content: center;
+	  background: #ffffff;
+	  border-radius:25upx;
+	  width:635upx;height:562upx;
+	  margin-top:100upx;
+	  margin-right:20upx;
+	  box-shadow: 20upx 20upx 0px 0 rgba(134,132,158,0.5);
+	  .ipt{
+		  border-radius:10upx!important;
+		  border:none!important;
+		  background-color: #ebeaf1;
+	  }
+  }
+  .ftbtn{
+	  margin:0 auto;
+	  .btn{
+		  background: #756ab6;
+		  border-radius:15upx;
+		  padding:10upx 0;
+		  font-size:28upx;
+	  }
   }
 </style>
